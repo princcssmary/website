bae5fdc8ed17ca014427a03c40d760b10605dddb
ignatmamedov <studentignat@gmail.com>
2022-03-19 22:44:42 +0300

    COM-0000: GOOGLE AUTH

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
-
+import pyotp
 
 auth = Blueprint('auth', __name__)
 
@@ -19,13 +19,36 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
-                return redirect(url_for('views.home'))
+                #return redirect(url_for('views.home'))
+                return redirect(url_for("auth.login_2fa_form"))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
 
     return render_template("login.html", user=current_user)
+
+
+@auth.route("/login/2fa/", methods=['GET', 'POST'])
+def login_2fa_form():
+    secret = pyotp.random_base32()
+    if request.method == 'POST':
+        # getting secret key used by user
+        secret = request.form.get("secret")
+        # getting OTP provided by user
+        otp = int(request.form.get("otp"))
+
+        # verifying submitted OTP with PyOTP
+        if pyotp.TOTP(secret).verify(otp):
+            # inform users if OTP is valid
+            flash("The TOTP 2FA token is valid", "success")
+            return redirect(url_for("auth.login_2fa_form"))
+        else:
+            # inform users if OTP is invalid
+            flash("You have supplied an invalid 2FA token!", "danger")
+            return redirect(url_for("auth.login_2fa_form"))
+
+    return render_template("login_2fa.html", secret=secret)
 
 
 @auth.route('/logout')
@@ -55,8 +78,10 @@ def sign_up():
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.', category='error')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(
-                password1, method='sha256'))
+            new_user = User(email=email,
+                            first_name=first_name,
+                            password=generate_password_hash(password1, method='sha256'),
+                            google_token=pyotp.random_base32())
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
